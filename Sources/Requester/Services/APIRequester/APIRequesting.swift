331787fed8e9c7b9bda54052829193c9314fb8fb
Kevin van den Hoek <k.vandenhoek@wearetriple.com>
2022-10-18 16:12:45 +0200

    expose requester

--- Sources/Requester/Services/APIRequester/APIRequesting.swift
+++ Sources/Requester/Services/APIRequester/APIRequesting.swift
@@ -9,6 +9,8 @@ import Foundation
 
 public protocol APIRequesting {
     
+    var memoryCacher: MemoryCaching { get }
+    
     @discardableResult func perform<Request: APIRequest>(_ request: Request) async throws -> Request.Response
     @discardableResult func performWithMemoryCaching<Request: APIRequest, Mapped>(_ request: Request, mapper: (Request.Response) throws -> Mapped) async throws -> Mapped
     @discardableResult func performWithMemoryCaching<Request: APIRequest, Mapped>(_ request: Request, maxCacheLifetime: TimeInterval?, mapper: (Request.Response) throws -> Mapped) async throws -> Mapped
